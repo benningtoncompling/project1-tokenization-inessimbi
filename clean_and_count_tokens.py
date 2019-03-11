c098d4b99745c3484c83e87a0cf6d6445d5ba982
inessimbi <inessimbi@bennington.edu>
2019-03-10 21:19:15 -0400

    added comments to files

--- clean_and_count_tokens.py
+++ clean_and_count_tokens.py
@@ -7,32 +7,18 @@
 
 import re
 
-
 file_name = "Wikipedia-LexicalAnalysis.xml"
 out_file_name = "lexical_analysis_out.txt"
 
 # use potter stemmer in each word before count
-counting_dict = {}
+counting_dict = {} # dictionary to save words
 with open(file_name, 'r') as file: # open file
    with open(out_file_name, 'w') as file_output: # writes to file
-        text = file.readlines()# read what's in file
-        #print(text)
-       # dictionary to save out words
-        # #loop to get lines in text
-        the_list= []
+        text = file.readlines() # read what's in file
+        the_list = []
         for line in text:
             line = line.lower() # Sadip suggested I make everything lowercase to be able to count words
-            #print(line)
-           # line = re.sub(r'()|()')
-            """
-            line = re.sub(r'<\w+>', " ", line)
-            line = re.sub(r'[<][/]\w+[>]', " ", line)
-            line = re.sub(r'[!^_]', " ", line)
-            """
             line = re.sub("[^a-zA-Z']+", " ", line)
-            #line = re.findall("\w+", line)
-            #line = re.sub("'(.+)", " ", line)
-            #line = re.sub(" (')+", " ", line)
             the_list = the_list + line.split() #adds lines in a list
         print(the_list)
         for word in the_list:
@@ -45,5 +31,4 @@ with open(file_name, 'r') as file: # open file
         for key in the_keys:
             the_value= counting_dict[key]
             the_string = key + ":" + str(the_value) + "\n" #adds key and it's value in string
-            #print(the_string)
             file_output.write(the_string) #outputs the string in a file
--- nltk_clean_and_count_stems.py
+++ nltk_clean_and_count_stems.py
@@ -11,30 +11,19 @@ from nltk.stem import PorterStemmer
 file_name = "Wikipedia-LexicalAnalysis.xml"
 out_file_name = "lexical_analysis_nltk_stemmed_out.txt"
 
-# use potter stemmer in each word before count
-counting_dict = {}
+counting_dict = {} # dictionary to save words
 with open(file_name, 'r') as file: # open file
    with open(out_file_name, 'w') as file_output: # writes to file
         text = file.readlines()# read what's in file
-        #print(text)
         the_stem = PorterStemmer()
-       # dictionary to save out words
-        # #loop to get lines in text
-        the_list= []
+        the_list = []
         for line in text:
             line = line.lower()
-            #print(line)
-           # line = re.sub(r'()|()')
-            """
-            line = re.sub(r'<\w+>', " ", line)
-            line = re.sub(r'[<][/]\w+[>]', " ", line)
-            line = re.sub(r'[!^_]', " ", line)
-            """
             line = re.sub("[^a-zA-Z']+", " ", line)
             the_list = the_list + line.split()
         print(the_list)
         for word in the_list:
-            stem = the_stem.stem(word)
+            stem = the_stem.stem(word) # stem word before counting and putting it in a dictionary
             if stem in counting_dict:
                 counting_dict[stem] = counting_dict[stem] + 1
             else:
@@ -45,5 +34,4 @@ with open(file_name, 'r') as file: # open file
             the_keys= sorted(counting_dict.keys())
             the_value= counting_dict[key]
             the_string = key + ":" + str(the_value) + "\n"
-            #print(the_string)
-            file_output.write(the_string)
+            file_output.write(the_string) 
